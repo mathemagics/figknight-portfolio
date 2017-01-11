a738f50977d30aafb0957f4d25e3b0b1672b6e15
Paul WB <higgsbison@gmail.com>
2017-01-10 21:43:16 -0800

    add carousel to main

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Slider from 'react-slick';
 import HeaderTitle from './header_title';
 
 export default () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <div className="main">
       <HeaderTitle>Fig Knight</HeaderTitle>
@@ -10,6 +18,14 @@ export default () => {
         <li>Mobile Apps</li>
         <li>User Experience</li>
       </ul>
+      <Slider {...settings}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
     </div>
   );
 };

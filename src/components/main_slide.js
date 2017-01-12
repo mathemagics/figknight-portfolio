1a50b74eb134386a587dafba51de2292810fd62e
Paul WB <higgsbison@gmail.com>
2017-01-11 20:11:46 -0800

    add media query

--- src/components/main_slide.js
+++ src/components/main_slide.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default (props) => {
   return (
     <div className="main-slide">
-      <img alt="phone" src={'https://s28.postimg.org/vy2plhjul/phone.png'} />
-      <div className="slide-text">{props.children}</div>
+      <div className="slide-contents">
+        <img alt="phone" src={'https://s28.postimg.org/vy2plhjul/phone.png'} />
+        <div className="slide-text">{props.children}</div>
+      </div>
     </div>
   );
 };
